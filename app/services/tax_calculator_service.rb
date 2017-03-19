b95c8e5ae3e1c3c32ad748d690a917ad0f22f6cd
Tim Morgan <tim@timmorgan.org>
2017-03-18 22:38:07 -0500

    Include shipping in taxable amount in some states

--- app/services/tax_calculator_service.rb
+++ app/services/tax_calculator_service.rb
@@ -4,10 +4,20 @@ class TaxCalculatorService
   end
 
   def call
-    taxjar.rates_for_location(@order.zip).combined_rate * @order.product_total / 100
+    taxjar.rates_for_location(@order.zip).combined_rate * taxable_amount / 100
   end
 
   private
+
+  SHIPPING_IS_TAXABLE_IN_THESE_STATES = %w(AR CO CT DC FL GA HI IL IN KS KY MI MN MS MO NE NJ NM NY NC ND OH PA RI SC SD TN TX VT WA WV WI)
+
+  def taxable_amount
+    if SHIPPING_IS_TAXABLE_IN_THESE_STATES.include?(@order.state)
+      @order.product_total + @order.shipping
+    else
+      @order.product_total
+    end
+  end
 
   def taxjar
     @taxjar ||= Taxjar::Client.new(api_key: ENV['TAXJAR_API_KEY'])
